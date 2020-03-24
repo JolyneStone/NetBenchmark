34b850e376e7cf2f094b2f18a4483d34e51377c3
zzq424 <997525106@qq.com>
2020-03-25 01:06:26 +0800

    update

--- NetBenchmark/Benchmark.cs
+++ NetBenchmark/Benchmark.cs
@@ -10,10 +10,24 @@ namespace NetBenchmark
         public static Runner Http(string name, IHttpClientFactory clientFactory, int connections, int interval, Func<IHttpHandler, Task> handler)
         {
             Runner runer = new Runner() { Interval = interval };
-            runer.Name = $"HTTP [Connections:{connections:###,###,###}]";
+            runer.Name = $"HTTP {name} [Connections:{connections:###,###,###}]";
             for (int i = 0; i < connections; i++)
             {
                 HttpTester tester = new HttpTester(clientFactory.CreateClient(name));
+                tester.Handler = handler;
+                tester.Runner = runer;
+                runer.Testers.Add(tester);
+            }
+            return runer;
+        }
+
+        public static Runner Http(IHttpClientFactory clientFactory, int connections, int interval, Func<IHttpHandler, Task> handler)
+        {
+            Runner runer = new Runner() { Interval = interval };
+            runer.Name = $"HTTP [Connections:{connections:###,###,###}]";
+            for (int i = 0; i < connections; i++)
+            {
+                HttpTester tester = new HttpTester(clientFactory.CreateClient());
                 tester.Handler = handler;
                 tester.Runner = runer;
                 runer.Testers.Add(tester);
